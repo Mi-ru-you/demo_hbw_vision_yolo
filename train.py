4197fa882bb6b610e9c1ddedfb14efe468a8fef9
unknown <1921706042@qq.com>
2026-06-01 00:55:36 +0800

    version3

--- train.py
+++ train.py
@@ -1,8 +1,4 @@
 from ultralytics import YOLO
 model = YOLO("yolo11n.pt")
-results = model.train(data="C:/Users/lenovo/Desktop/opencv/sixth/model_train/dataset/data.yaml",epochs=200,imgsz=640,
-    cls=1.5,
-    mosaic=0.5,
-    copy_paste=0.3,
-    lr0=0.005,
-)
+results = model.train(data="C:/Users/lenovo/Desktop/opencv/third/model_train/dataset/data.yaml", epochs=200, imgsz=640)
+results = model.val()

1c365148dbe1ddad86ab7a88c377597d2cfca4c1
unknown <1921706042@qq.com>
2026-06-01 01:16:06 +0800

    version6

--- train.py
+++ train.py
@@ -1,4 +1,12 @@
 from ultralytics import YOLO
 model = YOLO("yolo11n.pt")
-results = model.train(data="C:/Users/lenovo/Desktop/opencv/fifth/model_train/dataset/data.yaml", epochs=300, imgsz=640)
-results = model.val()
+results = model.train(data="C:/Users/lenovo/Desktop/opencv/sixth/model_train/dataset/data.yaml",epochs=200,imgsz=640,
+    cls=1.5,           # 提高分类损失权重
+    mosaic=0.5,        # 降低Mosaic增强
+    copy_paste=0.3,    # 开启复制粘贴增强
+    lr0=0.005,         # 降低学习率
+    seed=42,           # 固定种子
+)
+
+# 验证（用更严格的阈值评估）
+results = model.val(conf=0.6, iou=0.5)
